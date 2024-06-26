1105a66aaeb70dd1b3a08fca0466d7fb0750518e
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2024-06-26 13:29:29 +0300

    Codegen usage skeleton

--- src/callout.handlers/index.ts
+++ src/callout.handlers/index.ts
@@ -1,13 +1,14 @@
-import { AlkemioClient, Callout, CalloutType } from '@alkemio/client-lib';
+import { Callout, CalloutType } from '@alkemio/client-lib';
 import { Document } from 'langchain/document';
 import { baseHandler } from './base';
 import { linkCollectionHandler } from './link.collection';
+import { AlkemioCliClient } from 'src/graphql-client/AlkemioCliClient';
 
 const handlersMap: Record<
   CalloutType,
   (
     callout: Partial<Callout>,
-    alkemioClient: AlkemioClient | null
+    alkemioClient: AlkemioCliClient | null
   ) => Promise<Document[]>
 > = {
   [CalloutType.LinkCollection]: linkCollectionHandler,
@@ -19,7 +20,7 @@ const handlersMap: Record<
 
 export const handleCallout = async (
   callout: Partial<Callout>,
-  alkemioClient: AlkemioClient | null = null
+  alkemioClient: AlkemioCliClient | null = null
 ): Promise<Document[]> => {
   if (!callout.type) {
     throw new Error('Callout type is not part of query.');
---
+++ src/graphql-client/AlkemioCliClient.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { GraphQLClient } from 'graphql-request';
+import { Sdk, getSdk } from '../generated/graphql';
+import { Logger } from 'winston';
+import { AlkemioClient, AlkemioClientConfig } from '@alkemio/client-lib';
+import { UUID } from 'crypto';
+
+export class AlkemioCliClient {
+  public config!: AlkemioClientConfig;
+  public sdkClient!: Sdk;
+  public alkemioLibClient!: AlkemioClient;
+  public logger: Logger;
+
+  constructor(config: AlkemioClientConfig, logger: Logger) {
+    this.config = config;
+    this.logger = logger;
+    this.logger.info(`Alkemio server: ${config.apiEndpointPrivateGraphql}`);
+  }
+
+  async initialise() {
+    try {
+      this.alkemioLibClient = new AlkemioClient(this.config);
+      await this.alkemioLibClient.enableAuthentication();
+      const apiToken = this.alkemioLibClient.apiToken;
+
+      this.logger.info(`API token: ${apiToken}`);
+      const client = new GraphQLClient(this.config.apiEndpointPrivateGraphql, {
+        headers: {
+          authorization: `Bearer ${apiToken}`,
+        },
+      });
+      this.sdkClient = getSdk(client);
+    } catch (error) {
+      throw new Error(`Unable to create client for Alkemio endpoint: ${error}`);
+    }
+  }
+
+  async logUser() {
+    const userResponse = await this.sdkClient.me();
+    this.logger.info(
+      `Authenticated user: '${userResponse.data.me.user?.profile.displayName}'`
+    );
+  }
+
+  async validateConnection() {
+    return await this.alkemioLibClient.validateConnection();
+  }
+
+  public async ingestSpace(spaceID: string) {
+    const result = await this.sdkClient.spaceIngest({ spaceID });
+
+    return result.data;
+  }
+}
