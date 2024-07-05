978eff4d39da97d1dd1313dd047e5bc72a7db586
Vladimir Aleksiev <vladimir@alkem.io>
2024-07-05 16:32:49 +0300

    Improvements around service scalability (#41)
    * move batch size to env var with default of 20
    base winston log level on LOGGING_LEVEL variable and ensure its lower case
    auto ack messages on receival
    
    * address comments

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const CHUNK_SIZE = parseInt(process.env.CHUNK_SIZE || '1000');
+export const CHUNK_OVERLAP = parseInt(process.env.CHUNK_OVERLAP || '100');
+export const BATCH_SIZE = parseInt(process.env.BATCH_SIZE || '20');
